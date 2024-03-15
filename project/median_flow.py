7850bcbfcc383fc5e4abe84e5784d48142cec929
MidrarAdham <midrar@pdx.edu>
2024-03-14 17:09:43 -0700

    still working on the project

--- project/median_flow.py
+++ project/median_flow.py
@@ -6,9 +6,15 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 
 #%%
-
-filename = 'hands_stable_bg.mov'
+# filename = 'hands_stable.mov'
+# filename = 'hands_stable_bg.mov'
+# filename = 'hands_test.mov'
+filename = 'hands_test2.mov'
 cap = cv.VideoCapture(filename)
+
+def extract_background(frame):
+    backsub = cv.createBackgroundSubtractorKNN()
+    return backsub.apply(frame)
 
 def detect_skin(frame):
     # Convert BGR to YCbCr
@@ -23,37 +29,41 @@ def detect_skin(frame):
     
     # Apply morphological operations to remove noise
     # mask = cv.medianBlur(mask, 5)
-    mask = cv.GaussianBlur(mask, (5,5), 0)
+    mask = cv.GaussianBlur(mask, (5,5), 10)
     mask = cv.dilate(mask, None, iterations=2)
     mask = cv.erode(mask, None, iterations=2)
+
     
     return mask
 
 # Capture video from webcam
+backsub = cv.createBackgroundSubtractorKNN()
 
 while True:
     # Read a frame from the video stream
     ret, frame = cap.read()
     if not ret:
         break
-
-    # Detect skin in the frame
+    
+    frame = cv.resize(frame.copy(), (640, 800))
+    fg_frame = backsub.apply(frame)
+    # cv.imshow('Background', fg_frame)
+    
     skin_mask = detect_skin(frame)
+    cv.imshow('Skin Mask', skin_mask)
 
-    # Find contours in the skin mask
     contours, _ = cv.findContours(skin_mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-
-    # The largest contour is the hand
     if len(contours) > 0:
-        hand_contour = max(contours, key=cv.contourArea)
+        convex_hull = cv.convexHull(contours[0])
+        cv.drawContours(frame, [convex_hull], -1, (150, 155, 0), 2)
+        convexityDefects = cv.convexityDefects(contours[0],convex_hull)
+    
 
-        # Draw bounding box around the hand
-        x, y, w, h = cv.boundingRect(hand_contour)
-        cv.rectangle(frame, (x, y), (x + w, y + h), (20, 100, 150), 2)
 
     # Display the frame
     cv.imshow('Hand Tracking', frame)
-    # cv.imshow('Skin Mask', skin_mask)
+
+    # cv.imshow('Skin Mask', convexityDefects)
 
     if cv.waitKey(30) & 0xFF == ord('q'):
         break
